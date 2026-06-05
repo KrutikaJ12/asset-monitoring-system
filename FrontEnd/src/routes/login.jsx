59e4681749c829436039a6e89af66dfe5b327d5d
krutikaj <krutikats78@gmail.com>
2026-06-05 12:24:26 +0530

    feat:removed unused libraries and installation of react-icons

--- FrontEnd/src/routes/login.jsx
+++ FrontEnd/src/routes/login.jsx
@@ -2,8 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 import logo from "../assets/algo-logo.png";
 import bgImage from "../assets/backgroundImage.jpeg";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
-
+import { Eye,EyeOff } from 'lucide-react';
 export const Route = createFileRoute("/login")({
   component: Login,
 });
@@ -166,7 +165,7 @@ function Login() {
       onClick={() => setShowPassword(!showPassword)}
       className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
     >
-      {showPassword ? <FaEyeSlash size={16} /> : <FaEye size={16} />}
+      {showPassword ? <EyeOff size={16} /> : <Eye size={16} />}
     </button>
   </div>
 
